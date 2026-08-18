57676595d8a37f9c0e40e48be6f26188093bdf94
Amanor Evans <emperoramanor109@gmail.com>
2026-08-18 10:26:45 +0000

    ha

---
+++ project 7.js
@@ -0,0 +1,71 @@
+let numbers = [2,3,4]
+
+
+
+function sum (prev,next){
+    return prev + next
+}
+
+console.log(sum(8,9))
+
+console.log(numbers.reduce(sum))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const home_button = document.getElementById("home_btn");
+
+
+home_button.onclick = function (){
+    let leave_stay = window.prompt("Are you sure you want to go back to the home page? (y/n)");
+    if (leave_stay === "y") {
+        let rate = window.prompt("Rate this experience from 1 to 10");
+        rate = +rate;
+        if(typeof(rate) === "number" && rate >= 1 && rate <= 10) {
+            window.alert("Thank you for your feedback! You will now be redirected to the home page.");
+            window.location.href = "index.html";
+        }
+        else{
+            window.alert("Invalid rating! Please enter a number between 1 and 10.");
+            
+        }
+    }
+    else if (leave_stay === "n") {
+        window.alert("You will stay on the counter page.");
+        
+    }
+    else {
+        window.alert("Blind ass mother fucker!");
+    }
+
+
+}
