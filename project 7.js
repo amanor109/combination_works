64274bb781c615bcd58f1cdb2baf59eb69e45d13
Amanor Evans <emperoramanor109@gmail.com>
2026-08-28 21:45:24 +0000

    Now coming back

--- project 7.js
+++ project 7.js
@@ -1,71 +1,17 @@
-let numbers = [2,3,4]
+const numbers =[1, 2,3,4,5,6];
+console.log(numbers)
 
+const square_numbers = numbers.map(squares)
 
+numbers.forEach(squares)
+numbers.forEach(display_result)
 
-function sum (prev,next){
-    return prev + next
+function display_result(element){
+    console.log(element)
 }
 
-console.log(sum(8,9))
 
-console.log(numbers.reduce(sum))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const home_button = document.getElementById("home_btn");
-
-
-home_button.onclick = function (){
-    let leave_stay = window.prompt("Are you sure you want to go back to the home page? (y/n)");
-    if (leave_stay === "y") {
-        let rate = window.prompt("Rate this experience from 1 to 10");
-        rate = +rate;
-        if(typeof(rate) === "number" && rate >= 1 && rate <= 10) {
-            window.alert("Thank you for your feedback! You will now be redirected to the home page.");
-            window.location.href = "index.html";
-        }
-        else{
-            window.alert("Invalid rating! Please enter a number between 1 and 10.");
-            
-        }
-    }
-    else if (leave_stay === "n") {
-        window.alert("You will stay on the counter page.");
-        
-    }
-    else {
-        window.alert("Blind ass mother fucker!");
-    }
-
-
+function squares (element){
+    return Math.pow(element,2)
 }
+
